5a87f21770f4c513241c2a7f1b703812dfea1220
Evgeniy Pashko <evgpashko92@gmail.com>
2023-07-19 23:59:44 +0700

    просмотр постов по автору

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,8 @@
                 </a>
             </h2>
             <p>
-                <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
+                By <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> in <a
+                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
             </p>
             <div>
                 {!! $post->excerpt !!}
