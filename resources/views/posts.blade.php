c46e367e67e23a0edddffe5ea9edd2ab1019eaa9
Evgeniy Pashko <evgpashko92@gmail.com>
2023-07-29 00:12:50 +0700

    слияние стилей на компоненте карточки поста

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,13 +2,20 @@
     @include('_post-header')
 
     <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
-        <x-post-featured-card :post="$posts[0]" />
+        @if ($posts->count())
+            <x-post-featured-card :post="$posts[0]" />
 
-        <div class="lg:grid lg:grid-cols-2">
-            @foreach ($posts->skip(1) as $post)
-                <x-post-card :post="$post" />
-            @endforeach
-        </div>
+            @if ($posts->count() > 1)
+                <div class="lg:grid lg:grid-cols-6">
+                    @foreach ($posts->skip(1) as $post)
+                        <x-post-card :post="$post"
+                            class="{{ $loop->iteration < 3 ? 'col-span-3' : 'col-span-2' }}" />
+                    @endforeach
+                </div>
+            @endif
+        @else
+            <p class="text-center">No posts yet. Please check back later.</p>
+        @endif
 
         {{-- <div class="lg:grid lg:grid-cols-3">
             <x-post-card />
@@ -16,21 +23,4 @@
             <x-post-card />
         </div> --}}
     </main>
-
-    {{-- @foreach ($posts as $post)
-        <article>
-            <h2>
-                <a href="/posts/{{ $post->slug }}">
-                    {!! $post->title !!}
-                </a>
-            </h2>
-            <p>
-                By <a href="/authors/{{ $post->author->username }}">{{ $post->author->name }}</a> in <a
-                    href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a>
-            </p>
-            <div>
-                {!! $post->excerpt !!}
-            </div>
-        </article>
-    @endforeach --}}
 </x-layout>
