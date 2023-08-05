f2d17d46a5996b3384f8cde58bd39716e9ea4b68
Evgeniy Pashko <evgpashko92@gmail.com>
2023-08-05 15:39:25 +0700

    создание dropdown меню для выбора категории с помощью alpine js

--- routes/web.php
+++ routes/web.php
@@ -40,12 +40,14 @@ Route::get('posts/{post:slug}', function (Post $post) { // Post::where('slug', $
 
 Route::get('categories/{category:slug}', function (Category $category) {
     return view('posts', [
-        'posts' => $category->posts
+        'posts' => $category->posts,
+        'categories' => Category::all()
     ]);
 });
 
 Route::get('authors/{author:username}', function (User $author) {
     return view('posts', [
-        'posts' => $author->posts
+        'posts' => $author->posts,
+        'categories' => Category::all()
     ]);
 });
