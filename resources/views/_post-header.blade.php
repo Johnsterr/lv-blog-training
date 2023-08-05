2f91e1e13baa9251878c08871f0265d58cd5e880
Evgeniy Pashko <evgpashko92@gmail.com>
2023-08-05 15:49:35 +0700

    идентификация выбранной категории в dropdown меню

--- resources/views/_post-header.blade.php
+++ resources/views/_post-header.blade.php
@@ -16,7 +16,7 @@
             <div x-data="{ show: false }" @click.away="show = false">
                 <button class="py-2 pl-3 pr-9 text-sm font-semibold w-full lg:w-32 text-left flex lg:inline-flex"
                     @click="show = !show">
-                    Categories
+                    {{ isset($currentCategory) ? ucwords($currentCategory->name) : 'Categories' }}
 
                     <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
                         height="22" viewBox="0 0 22 22">
@@ -31,33 +31,25 @@
                 </button>
 
                 <div class="py-2 absolute bg-gray-100 mt-2 rounded-xl w-full z-50" style="display: none" x-show="show">
+                    <a href="/"
+                        class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                        All
+                    </a>
                     @foreach ($categories as $category)
                         <a href="/categories/{{ $category->slug }}"
-                            class="block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500 hover:text-white focus:text-white">
+                            class="
+                            block text-left px-3 text-sm leading-6 hover:bg-blue-500 focus:bg-blue-500
+                            hover:text-white focus:text-white
+                            {{-- 1ый вариант для идентификации выбранной категории --}}
+                            {{ isset($currentCategory) && $currentCategory->is($category) ? 'bg-blue-500 text-white' : '' }}
+                            {{-- 2ой вариант для идентификации выбранной категории --}}
+                            {{-- {{ isset($currentCategory) && $currentCategory->id === $category->id ? 'bg-blue-500 text-white' : '' }} --}}
+                            ">
                             {{ ucwords($category->name) }}
                         </a>
                     @endforeach
                 </div>
             </div>
-
-            {{-- <select class="flex-1 appearance-none bg-transparent py-2 pl-3 pr-9 text-sm font-semibold">
-                <option value="category" disabled selected>
-                    Category
-                </option>
-                @foreach ($categories as $category)
-                    <option value="{{ $category->slug }}">{{ $category->name }}</option>
-                @endforeach
-            </select>
-
-            <svg class="transform -rotate-90 absolute pointer-events-none" style="right: 12px;" width="22"
-                height="22" viewBox="0 0 22 22">
-                <g fill="none" fill-rule="evenodd">
-                    <path stroke="#000" stroke-opacity=".012" stroke-width=".5" d="M21 1v20.16H.84V1z">
-                    </path>
-                    <path fill="#222" d="M13.854 7.224l-3.847 3.856 3.847 3.856-1.184 1.184-5.04-5.04 5.04-5.04z">
-                    </path>
-                </g>
-            </svg> --}}
         </div>
 
         <!-- Other Filters -->
